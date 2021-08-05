b30301433d0c79bbbd2f4f4069cecfb9dee746c2
winter <1304879929@qq.com>
2021-08-05 14:26:30 +0800

    Vue单文件组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+// import Vue from 'vue'
+// import App from './App.vue'
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Demo from './Demo.vue'
+// console.log(Demo);
+console.log(Demo.render.toString());
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render(h) {
+        return h(Demo)
+    }
+})
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
