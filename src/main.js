ffa41ad79b208275326ac219e8f842ad0d3a5a79
winter <1304879929@qq.com>
2021-08-06 15:47:51 +0800

    el可以用()代替

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
-// import Vue from 'vue'
-// import App from './App.vue'
+
 import Vue from 'vue'
 import Demo from './Demo.vue'
-// console.log(Demo);
-console.log(Demo.render.toString());
+
 
 new Vue({
-    el: '#app',
+    // el: '#app',
     render(h) {
         return h(Demo)
     }
-})
+}).$mount('#app')
 
-// Vue.config.productionTip = false
-
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
